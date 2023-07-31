ef26377c5b7fdffd639d1d2411e142697a963fcc
KonstantinKozhemiakin <kozhemiakin.konstantin@gmail.com>
2023-07-31 15:08:00 +0300

    add history of assistant

--- main.py
+++ main.py
@@ -14,52 +14,13 @@ dp = Dispatcher(bot)
 
 messages = [
     {"role": "system",
-     "content": '''As a recruiter, you should conduct multiple-choice tests to assess the knowledge and skills of a Lead Generation Manager candidate. 
-Ask random questions on these topics:
-		Lead Generation Manager / Researcher
-        International sales of software services
-        Outstaff and Outsource
-        LinkedIn and Email outreach
-        Software development stages (Web and Mobile)
-        Written English (Intermediate)
-        Attention to detail
-        Automation and data management tools (Apollo, Instantly, Zaper, Linked-helper, etc.)
-        Tech stack
-        IT Outsourcing company
-        LinkedIn Sales Navigator, Clutch, Upwork, PipeDrive
-        English level B2+ or C1
-        Lead-gen (outstaffing / outsourcing)
-        Lead-gen tools
-        Email outreach
-        IT technologies
-        B2B sales experience
-        CRM systems
-        Upwork, FB, AngelList, PPH
-        Automation tools (Snovio, GrowthLead, Lemlist, etc.)
-        Presales Mastery
-        Google Workspace, Snov.io, Findthatlead, Crunchbase
-        IT lead generator
-        Black humor and sarcasm
-        B2B CSM experience
-        GigRadar, Upwork
-        SDLC (Software Development Life Cycle)
-        English (Upper-Intermediate or above)
-        Result-driven
-        ICP (Ideal Customer Profile)
-        IT outsourcing services
-        B2B lead generation
-        CRM management
-        Market research and trends
-        Team management
-        Analytical skills
-        Goal-driven and results-oriented.
-After 10 answers avaluate the candidate by the percentage of correct answers'''},
+     "content": "You are a recruiter who is testing a candidate for the position of lead generator. Create a list of different questions on topics such as: Online marketing, Lead generation, Search engine optimization (SEO), Pay-per-click (PPC) advertising, Social media marketing, Website design and development, Customer relationship management (CRM). For each question, create a multiple-choice answer with one correct answer and two or three incorrect answers. Ask questions from the list in turn, accepting one answer at a time. After the candidate completes 10 tests, evaluate his performance by calculating the percentage of correct answers on the ten questions."},
     {"role": "user",
      "content": "I am a candidate for the position of lead generator, I want you to determine my level of knowledge"},
     {"role": "assistant", "content": "Greetings! Are you ready to take the test?"}]
 
 
-def update(message, role, content):
+def update(messages, role, content):
     messages.append({"role": role, "content": content})
     return messages
 
@@ -71,14 +32,16 @@ async def send_welcome(message: types.Message):
 
 @dp.message_handler()
 async def send(message: types.Message):
-    update(messages, "user", message.text)
+    global messages
+    messages = update(messages, "user", message.text)
+    print(messages)
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
-        #temperature=0.9
-
+        # temperature=0.1
     )
     await message.answer(response['choices'][0]['message']['content'])
+    messages = update(messages, "assistant", response['choices'][0]['message']['content'])
 
 
 if __name__ == '__main__':
